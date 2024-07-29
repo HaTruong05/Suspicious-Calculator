586315f16394c9ee536d33701e56278e2c9d68e9
HaTruong05 <125408638+HaTruong05@users.noreply.github.com>
2024-07-29 15:44:14 -0400

    Prevent division by 0. Improve display.
    If the user attempts to divide by 0, a message will display warning them before resetting the calculator.
    
    If the screen is displaying 0, inputting 0 doesn't result in a leading 0.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ TMP = null;
 OPERATORS = new Set(['+', '-', '*', '/']);
 EQ = '=';
 CLR = 'AC';
+CAN_INTERACT = true;
 
 function add(a, b){
     return a + b;
@@ -22,6 +23,23 @@ function divide(a, b){
     return a / b;
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function divisionError(ms) {
+    CAN_INTERACT = false;
+    await sleep (500);
+    document.querySelector(".screen").innerText = "(｡•ˇ‸ˇ•｡)";
+    await sleep (ms);
+    document.querySelector(".screen").innerText = "You can't divide by 0";
+    await sleep (ms);
+    document.querySelector(".screen").innerText = "Clearing memory...";
+    await sleep (ms);
+    document.querySelector(".screen").innerText = FIRST;
+    CAN_INTERACT = true;
+}
+
 function operate(a, b, op){
     a = parseFloat(a);
     b = parseFloat(b);
@@ -32,6 +50,10 @@ function operate(a, b, op){
     } else if (op === '*'){
         return multiply(a, b);
     } else {
+        if (b === 0) { /*prevent division by 0*/
+            divisionError(1000);
+            return 0;
+        }
         return divide(a, b);
     }
 }
@@ -42,42 +64,47 @@ function addDetect(){
 }
 
 function getVal(event){
-    button = event.target.innerText;
-    console.log(button);
-    if (button === EQ) {/*2 numbers and operator exist, calculate output*/
-        if (SEC != null){
-            res = operate(FIRST, SEC, OPERATOR);
-            document.querySelector(".screen").innerText = res;
-            FIRST = res;
-            SEC = null;
+    if (CAN_INTERACT) {
+        button = event.target.innerText;
+        if (button === EQ) {/*2 numbers and operator exist, calculate output*/
+            if (SEC != null){
+                res = operate(FIRST, SEC, OPERATOR);
+                document.querySelector(".screen").innerText = res;
+                FIRST = res;
+                SEC = null;
+                OPERATOR = null;
+            }
+        } else if (button === CLR) {
+            FIRST = null;
             OPERATOR = null;
-        }
-    } else if (button === CLR) {
-        FIRST = null;
-        OPERATOR = null;
-        SEC = null;
-        TMP = null;
-        document.querySelector(".screen").innerText = 0;
-    } else if (OPERATORS.has(button)) {
-        if (FIRST != null && SEC != null){
-            FIRST = operate(FIRST, SEC, OPERATOR)
-            SEC = null
-            document.querySelector(".screen").innerText = FIRST;
-        }
-        OPERATOR = button;
-    } else { /*pressing one of the numbers*/
-        if (FIRST === null) {
-            FIRST = button;
-            document.querySelector(".screen").innerText = FIRST;
-        } else if (OPERATOR === null) {
-            FIRST += button;
-            document.querySelector(".screen").innerText += button;
-        } else if (SEC === null) {
-            SEC = button;
-            document.querySelector(".screen").innerText = SEC;
-        } else {
-            SEC += button;
-            document.querySelector(".screen").innerText = SEC;
+            SEC = null;
+            TMP = null;
+            document.querySelector(".screen").innerText = 0;
+        } else if (OPERATORS.has(button)) {
+            if (FIRST != null && SEC != null){
+                FIRST = operate(FIRST, SEC, OPERATOR)
+                SEC = null
+                document.querySelector(".screen").innerText = FIRST;
+            }
+            OPERATOR = button;
+        } else { /*pressing one of the numbers*/
+            if (FIRST === null) {
+                FIRST = button;
+                document.querySelector(".screen").innerText = FIRST;
+            } else if (OPERATOR === null) {
+                if (parseFloat(FIRST) === 0) {
+                    FIRST = button;  
+                } else {
+                    FIRST += button;
+                }
+                document.querySelector(".screen").innerText = FIRST;
+            } else if (SEC === null || parseFloat(SEC) === 0) {
+                SEC = button;
+                document.querySelector(".screen").innerText = SEC;
+            } else {
+                SEC += button;
+                document.querySelector(".screen").innerText = SEC;
+            }
         }
     }
 }
